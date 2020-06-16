6c2a1e5f6a4d10733ef9d69736e77916a7c41c2e
Devanshi Singh <devanshisingh6b@gmail.com>
2020-06-16 19:38:09 +0530

    added files

---
+++ script.js
@@ -0,0 +1,20 @@
+//alert("hello");
+
+function fun()
+{
+  alert("hello");
+  //index.getElementById("aboutme").innerHTML = "haha";
+}
+
+function scrollWin()
+{
+  alert("World");
+  var elmnt = document.getElementById("aboutme");
+  elmnt.scrollIntoView();
+  //console.log('Hello');
+}
+
+// function qwe()
+// {
+//   $('#aboutme').html('TYU');
+// }
